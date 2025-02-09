40435a4d70737548388848aab345dc12b5a84688
hemosoul <hemosoul@foxmail.com>
2025-02-09 10:37:48 +0800

    fix

--- src/main/index.ts
+++ src/main/index.ts
@@ -20,6 +20,7 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
+    mainWindow.maximize()
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
